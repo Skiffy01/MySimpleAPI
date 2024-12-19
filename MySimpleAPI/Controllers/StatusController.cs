17c82f729581e05d2d47dfd1c6f2737df37ee131
Aaron Skiftun Dalbak <aaron.skiftun-dalbak@capgemini.com>
2024-12-19 14:43:56 +0100

    Add project files.

---
+++ MySimpleAPI/Controllers/StatusController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MySimpleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+
+        [HttpGet("GetStatus")]
+        public async Task<IActionResult> GetStatus()
+        {
+
+            return Ok();
+        }
+    }
+}
